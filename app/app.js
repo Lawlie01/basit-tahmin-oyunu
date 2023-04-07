a20b06b19d8e787ea0cf3ef76ec30020a3acce22
Lawlie <ahmetsadikoglu977@gmail.com>
2023-04-07 14:20:39 +0300

    tamamlandi

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 
-
+"use scrict";
 let hakSayisi = 0;
 let rastgeleSayi = 0;
-let _tahminEt = document.getElementById("tahminEt");
-let _yeniOyun = document.getElementById("yeniOyun");
-let _sonuc = document.getElementById("sonuc");
-let _sayi = document.getElementById("sayi");
+const _tahminEt = document.getElementById("tahminEt");
+const _yeniOyun = document.getElementById("yeniOyun");
+const _sonuc = document.getElementById("sonuc");
+const _sayi = document.getElementById("sayi");
 
 _yeniOyun.onclick = yeni;
 _tahminEt.onclick = tahmin;
@@ -14,3 +14,37 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
+function yeni() {
+    rastgeleSayi = randomInt(0, 99);
+    hakSayisi = 10;
+    _sonuc.innerHTML = "";
+    _sayi.value = "";
+    _tahminEt.disabled = false;
+    _yeniOyun.disabled = true;
+}
+
+function tahmin() {
+    if(hakSayisi == 0) return;
+    let girilenSayi = parseInt(_sayi.value);
+    let mesaj = "";
+    if (isNaN(girilenSayi)) return;
+
+    if(rastgeleSayi > girilenSayi) {
+        mesaj = "Daha BÜYÜK değer deneyin";
+    } else if (girilenSayi > rastgeleSayi){
+        mesaj = "Daha KÜÇÜK değer deneyin";
+    } else {
+        mesaj = "Tebrikler!!!";
+        _tahminEt.disabled = true;
+        _yeniOyun.disabled = false;
+    }
+    
+    hakSayisi--;
+    _sonuc.innerHTML = mesaj
+    _sonuc.innerHTML += "<br />"
+    _sonuc.innerHTML += "Kalan hak sayısı : " + hakSayisi
+    if (hakSayisi == 0) {
+        _tahminEt.disabled = true;
+        _yeniOyun.disabled = false;
+    }
+}
